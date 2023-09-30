06f327ba0ad0c75bda5e7f8b5a4d4924cc858b18
william <williamgonzagadasantacruz@gmail.com>
2023-09-30 12:01:14 -0300

    Commit Terminal

---
+++ metodos-sistema-smar-tv/src/Usuario.java
@@ -0,0 +1,42 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTV smartTV = new SmartTV();
+
+        smartTV.diminuirVolume();  
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.aumentarVolume();
+        System.out.println("Volume Atual : " + smartTV.volume);
+
+
+        System.out.println("Canal Atual : " + smartTV.canal);
+
+        smartTV.mudarCanal(13);
+        System.out.println("Volime Atual : " + smartTV.volume);
+
+        System.out.println("TV Ligada ? " + smartTV.ligada);
+        System.out.println("Canal Atual : " + smartTV.canal);
+        System.out.println("Volume Atual : " + smartTV.volume);
+
+
+
+        
+
+        smartTV.ligar ();
+        System.out.println("Novo Status TV Ligada ? " + smartTV.ligada);
+
+        smartTV.desligar();
+        System.out.println("Novo Status TV Ligada ? " + smartTV.ligada);
+
+
+        smartTV.aumentarVolume();
+        System.out.println("Novo Status Volume Atual : " + smartTV.volume);
+
+        smartTV.diminuirVolume();
+        System.out.println("Novo Status Volume Atual : " + smartTV.volume);
+
+
+
+    }
+}
